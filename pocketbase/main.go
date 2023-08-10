f1057222c13995b2137e935a70164a98865d78f6
H3rmt <stemmer.enrico@gmail.com>
2023-08-10 19:27:28 +0200

    updated pocketbase to support download; added size endpoint

--- pocketbase/main.go
+++ pocketbase/main.go
@@ -2,38 +2,39 @@ package main
 
 import (
 	"fmt"
-	"github.com/pocketbase/pocketbase"
-	"github.com/pocketbase/pocketbase/core"
-	"github.com/pocketbase/pocketbase/tools/filesystem"
+	"io/fs"
 	"log"
+	"net/http"
+	"path/filepath"
+	"strings"
+
+	"github.com/labstack/echo/v5"
+	"github.com/pocketbase/pocketbase"
+	"github.com/pocketbase/pocketbase/apis"
+	"github.com/pocketbase/pocketbase/core"
 )
 
 func main() {
 	app := pocketbase.New()
 
-	//app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
-	//	e.Router.AddRoute(echo.Route{
-	//		Method: http.MethodGet,
-	//		Path:   "/api/hello",
-	//		Handler: func(c echo.Context) error {
-	//			return c.String(http.StatusOK, "Hello world!")
-	//		},
-	//		Middlewares: []echo.MiddlewareFunc{
-	//			apis.ActivityLogger(app),
-	//			apis.RequireAdminAuth(),
-	//		},
-	//	})
-	//
-	//	return nil
-	//})
-
-	app.OnRecordBeforeCreateRequest().Add(func(e *core.RecordCreateEvent) error {
-		// overwrite the newly submitted "posts" record status to pending
-		if e.Record.Collection().Name == "files" {
-			fmt.Print("file record created")
-			fmt.Print(e.UploadedFiles)
-			e.UploadedFiles = make(map[string][]*filesystem.File)
-		}
+	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
+		e.Router.GET("/size", func(c echo.Context) error {
+			var totalSize int64
+			filepath.WalkDir("pb_data/storage", func(path string, d fs.DirEntry, err error) error {
+				if !d.IsDir() {
+					info, err := d.Info()
+					if err != nil {
+						log.Print(err)
+						return c.String(http.StatusInternalServerError, "")
+					}
+					if !strings.HasSuffix(info.Name(), ".attrs") {
+						totalSize += info.Size()
+					}
+				}
+				return nil
+			})
+			return c.String(http.StatusOK, fmt.Sprint(totalSize))
+		}, apis.ActivityLogger(app))
 
 		return nil
 	})
