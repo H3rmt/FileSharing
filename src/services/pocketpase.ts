1c0123bd5c25fd6a2644f5d91ac196106c185fb8
Enrico Stemmer <stemmer.enrico@gmail.com>
2023-10-25 08:01:57 +0000

    faster redirect to login

--- src/services/pocketpase.ts
+++ src/services/pocketpase.ts
@@ -3,6 +3,9 @@ import Pocketbase from "pocketbase";
 export const pb = new Pocketbase();
 
 export async function checkLogin() {
+  if (!pb.authStore.model) {
+    window.location.href = "/login";
+  }
   try {
     await pb.collection("users").authRefresh();
   } catch (e) {
